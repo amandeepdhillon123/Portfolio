d4c357633d37b324435e6cc7ccc95592ff5ab584
amandeepdhillon123 <amandeep24397@gmail.com>
2023-07-24 11:27:53 +0530

    home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './Home.css'
+import Resume from "../assets/docs/resume.txt"
+import Typewriter from 'typewriter-effect';
 const Home = () => {
   return (
-    <div>
-        <h1>home page</h1>
-    </div>
+    <>
+        <div className="container-fluid home-container">
+          <div className='container home-content'>
+            <h1>Hi I'm a </h1>
+            <h2>
+            <Typewriter 
+                options={{
+                  strings: ['FullStack Developer !', 'Mern Stack Developer !'],
+                  autoStart: true,
+                  loop: true,
+                }}
+            />
+            </h2>
+            <div className='home-buttons'>
+                    <button className='btn btn-hire'>Hire Me</button>
+                    <a className='btn btn-cv' href={Resume} download="your_name.pdf">My Resume</a>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
