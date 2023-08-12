cecf9f42c7191ba64ad9f0a72b7dfd98b7b2a0bf
amandeepdhillon123 <amandeep24397@gmail.com>
2023-08-12 18:57:04 +0530

    about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -12,11 +12,7 @@ const About = () => {
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
             <h1>About me</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              officiis commodi illo nihil culpa voluptates! Placeat corporis
-              pariatur laboriosam nostrum sunt, ullam necessitatibus impedit
-              quaerat deleniti illo qui dolorem perspiciatis? ut itaque
-              doloribus. .
+            Hello there! I'm Amandeep. I have done B.tech in mechanical branch from Indus Institue of Engineering and Technology (Kurukshetra University). I am a versatile MERN stack web developer with a mission to bring innovative ideas to life through seamless digital experiences. I've mastered a range of technologies including HTML, CSS, JavaScript, React.js, and Next.js, as well as backend technologies Node.js Express.js and Database mongoDB. This dual expertise enables me to create holistic solutions that elevate user engagement and business goals.With a strong foundation in the MERN technology stack, I specialize in creating robust, responsive, and user-friendly web applications.
             </p>
           </div>
         </div>
